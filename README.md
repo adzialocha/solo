f67915ba28f8588fcd58e13302e0328371bbaf28
Andreas Dz <andreas@marmorkuchen.net>
2015-03-26 21:34:02 +0100

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # channel-toggle-interface
+
 Channel Toggle Matrix Interface and Editor for OSC
---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+(function(window, undefined) {
+
+  // public
+
+  var app = {};
+
+  app.init = function() {
+
+  };
+
+  window.app = window.app || app;
+
+})(window);
